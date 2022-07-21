bbf779065071fab3c11342f25b44469e6bd6a03a
Rizki DS <dsrizki@192.168.1.4>
2022-07-21 11:19:44 +0700

    change baseUrl

--- src/stores/allState.js
+++ src/stores/allState.js
@@ -2,7 +2,8 @@ import { defineStore } from 'pinia';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
-const baseUrl = 'http://localhost:3000';
+// const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://disc-ukm-iproject-server.herokuapp.com';
 
 export const useAllStateStore = defineStore({
   id: 'allState',
