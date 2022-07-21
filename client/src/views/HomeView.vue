154a6bc0adbc30427821d14e63585f30c73d2c90
alirezaG0104 <alireza0104@gmail.com>
2022-07-22 05:08:23 +0700

    homepage -1

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,21 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<script >
+import preaches from '@/components/preaches.vue'
+import today from '@/components/today.vue'
 import { RouterLink, RouterView } from 'vue-router'
-
+export default {
+  components:{
+    RouterLink,
+    preaches,
+    today
+  }
+}
 </script>
 
 <template>
-
-  <main>
-    <TheWelcome />
-  </main>
+<h1 class="text-3xl font-bold text-center mt-5">Today's Preaches</h1>
+<today/>
+<h1 class="text-3xl font-bold text-center">All Preaches</h1>
+ <preaches/>
 
 
 </template>
