144779b5291825e109d7652c54302e33215c6a56
rivaldiheriyan27 <60634705+rivaldiheriyan27@users.noreply.github.com>
2022-07-21 17:05:14 +0700

    Mapbox thirdparty
    Penggunaan map box bisa dilakukan

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,74 +1,40 @@
 <script>
-import Mapbox from "mapbox-gl";
-// import { MglMap } from "vue-mapbox";
+import axios from "axios";
 import Navbar from "../components/Navbar.vue";
-// import { mapOptions } from "../assets/data";
+import leaflet from "leaflet";
+import { onMounted } from "vue";
 
 export default {
   name: "Home",
   components: {
     Navbar,
-    Mapbox,
   },
-
-  data() {
-    return {
-      accessToken:
-        "pk.eyJ1Ijoicml2YWxkaTEyIiwiYSI6ImNsNXMwbzRpczJkaTgzY3JvMDd3YnJidGQifQ.XD3HwgaRxr5f4JV0U2SwMw", // your access token. Needed if you using Mapbox maps
-      mapStyle: "mapbox://styles/kpfui/ckn9c157p02a618rzglws1mum", // your map style
-    };
-  },
-
-  methods: {
-    async onMapLoad(event) {
-      // Here we cathing 'load' map event
-      const asyncActions = event.component.actions;
-
-      const newParams = await asyncActions.flyTo({
-        center: [-6.168495573888538, 106.63818936456171],
-        zoom: 16,
-        speed: 1,
-      });
-    },
-  },
-  created() {
-    // We need to set mapbox-gl library here in order to use it in template
-    this.mapbox = Mapbox;
+  setup() {
+    let map;
+    onMounted(() => {
+      //init map
+      map = leaflet
+        .map("map")
+        .setView([-6.168495573888538, 106.63818936456171], 15);
+      //addd ti;e layer
+      leaflet
+        .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 19,
+          attribution: `Map data &copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors `,
+        })
+        .addTo(map);
+    });
   },
 };
-// let map = null;
-
-// onMounted(() => {
-//   mapboxgl.accessToken =
-//     "pk.eyJ1Ijoicml2YWxkaTEyIiwiYSI6ImNsNXMwbzRpczJkaTgzY3JvMDd3YnJidGQifQ.XD3HwgaRxr5f4JV0U2SwMw";
-//   map = new mapboxgl.Map(mapOptions);
-//   map.on("load", () => {
-//     map.addLayer({
-//       id: "terrain-data",
-//       type: "line",
-//       source: {
-//         type: "vector",
-//         url: "mapbox://mapbox.mapbox-terrain-v2",
-//       },
-//       "source-layer": "contour",
-//     });
-//   });
-// });
-// function flyTo(coords) {
-//   map.flyTo({
-//     center: [coords[0], coords[1]],
-//     zoom: 17,
-//     speed: 1,
-//     curve: 1.2,
-//     easing(t) {
-//       return t;
-//     },
-//   });
 </script>
 
 <template>
   <Navbar />
-  <Mapbox :accessToken="accessToken" :mapStyle="mapStyle" />
+  <div id="map"></div>
 </template>
 
-<style></style>
+<style>
+#map {
+  height: 380px;
+}
+</style>
