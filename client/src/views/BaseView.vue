33b9aec193dba2c8b60157e471d81359ca3b8b59
alirezaG0104 <alireza0104@gmail.com>
2022-07-22 02:09:08 +0700

    template added & edited

--- client/src/views/BaseView.vue
+++ client/src/views/BaseView.vue
@@ -12,80 +12,20 @@ export default {
 
 <template>
  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <nav class="flex items-center justify-between">
+                <h2 class="text-gray-200 font-bold text-2xl ">Home</h2>
+                <div class="auth flex items-center">
+                    <button class="bg-transparent text-gray-200  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700">Sign in</button>
+                    <button class="bg-gray-900 text-gray-200  py-2 px-3 rounded  hover:bg-gray-800 hover:text-gray-100">Sign up for free</button>
+        <!-- <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Home</RouterLink> -->
+                </div>
+            </nav>
+      <!-- <nav>
+      </nav> -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
 
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,3 +1,35 @@
 <template>
-<h1> Login PAGEEEEEEEEEE</h1>
+<header>
+        <div class="content px-12 py-2">
+            <div class="body mt-20 mx-12" style="margin-top: 32vh">
+                <div class="md:flex items-center justify-between">
+                    <div class="w-full md:w-1/2 mr-auto" style="text-shadow: 0 20px 50px hsla(0,0%,0%,8);">
+                        <h1 class="text-4xl font-bold text-white tracking-wide">Pray</h1>
+                        <h2 class=" text-2xl font-bold text-white tracking-wide">Shalom Saudara-saudari terkasih</h2>
+                        <p class="text-gray-300">
+                                Mari meluangkan waktu sejenak setiap hari untuk memberikan kita motivasi kepada diri sendiri. Belajar merefleksikan diri dari firman Tuhan. Mendengarkan pesan Allah kepada kita hari ini. Mendatangkan hari sejaterah di hari kita. Daftar diri agar kami dapat membantu anda dekat menjadi lebih baik lagi.
+                        </p>
+                        <span class="text-white">Create New Account?<a href="#" class="text-gray-900 text-lg ml-2 font-bold">Sign Up</a></span>
+                    </div>
+                    <div class="w-full md:max-w-md mt-6">
+                        <div class="card bg-white shadow-md rounded-lg px-4 py-4 mb-6 ">
+                            <form action="#">
+
+                                <h2 class="text-xl text-center font-semibold text-gray-800 mb-2">
+                                    Sign In
+                                </h2>
+                                <input type="text" class="rounded px-4 w-full py-1 bg-gray-200  border border-gray-400 mb-6 text-gray-700 placeholder-gray-700 focus:bg-white focus:outline-none" placeholder="Email Address">
+                                <input type="password" class="rounded px-4 w-full py-1 bg-gray-200  border border-gray-400 mb-4 text-gray-700 placeholder-gray-700 focus:bg-white focus:outline-none" placeholder="Password">
+                                <div class="flex items-center justify-between">
+                                    <a href="#" class="text-gray-600">Forget Password?</a>
+                                    <button class="bg-gray-800 text-gray-200  px-2 py-1 rounded">Sign In</button>
+                                </div>
+                            </form>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
